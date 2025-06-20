7a743f1c7a46404990476deb41f02297ddd0f0c9
ian <ianwisdom90@gmail.com>
2025-06-20 07:12:32 +0300

    restful ud

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -16,14 +14,14 @@ db.init_app(app)
 
 api = Api(app)
 
-class Home(Resource):
+class Index(Resource):
 
     def get(self):
-        
+
         response_dict = {
-            "message": "Welcome to the Newsletter RESTful API",
+            "index": "Welcome to the Newsletter RESTful API",
         }
-        
+
         response = make_response(
             response_dict,
             200,
@@ -31,12 +29,12 @@ class Home(Resource):
 
         return response
 
-api.add_resource(Home, '/')
+api.add_resource(Index, '/')
 
 class Newsletters(Resource):
 
     def get(self):
-        
+
         response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
 
         response = make_response(
@@ -47,7 +45,7 @@ class Newsletters(Resource):
         return response
 
     def post(self):
-        
+
         new_record = Newsletter(
             title=request.form['title'],
             body=request.form['body'],
@@ -76,6 +74,40 @@ class NewsletterByID(Resource):
         response = make_response(
             response_dict,
             200,
+        )
+
+        return response
+
+    def patch(self, id):
+
+        record = Newsletter.query.filter_by(id=id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
+    def delete(self, id):
+
+        record = Newsletter.query.filter_by(id=id).first()
+
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message": "record successfully deleted"}
+
+        response = make_response(
+            response_dict,
+            200
         )
 
         return response
